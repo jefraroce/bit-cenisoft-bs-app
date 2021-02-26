ec6b2533929d60ee392f359c47aa52c3ff580ca4
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2021-02-25 22:20:41 -0500

    Adjusted app layouts and routes.

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  count = 0;
+  isAuthenticated = false;
 
-  constructor() { }
+  constructor(
+    private tokenService: TokenService,
+    private router: Router,
+    private cartService: CartService
+    ) { }
 
   ngOnInit(): void {
+    this.tokenService.tokenObservable.subscribe((token) => {
+      this.isAuthenticated = token ? true : false;
+    });
+
+    this.cartService.cartObservable.subscribe((cart) => {
+      this.count = cart.reduce((a, b) => a + b.quantity, 0);
+    })
   }
 
+  signOut() {
+    this.tokenService.removeToken();
+    this.router.navigate(['/home']);
+  }
 }
---
+++ src/app/layouts/without-banner/without-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-without-banner',
+  templateUrl: './without-banner.component.html',
+  styleUrls: ['./without-banner.component.scss']
+})
+export class WithoutBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
