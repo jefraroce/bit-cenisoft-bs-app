2c1125cdcc5cfd582b156d71fc176ad367811aed
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2021-02-25 22:21:56 -0500

    Implemented sign in component.

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { TokenService } from 'src/app/services/token.service';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
+  signInForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authenticationService: AuthenticationService,
+    private tokenService: TokenService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    this.signInForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
+  onSubmit() {
+    if (this.signInForm.valid) {
+      const { email, password } = this.signInForm.value;
+      this.authenticationService.signInWithEmailAndPassword(email, password)
+        .subscribe(
+          (token: any) => {
+            console.log('token ', token);
+            this.tokenService.saveToken(token.jwt);
+            this.router.navigate(['/home']);
+          },
+          (response) => {
+            swal('Error', 'Correo Electrónico o Contraseña invalidos.', 'error');
+            console.error('Error authenticating client: ', response);
+          }
+        );
+    } else {
+      console.log('form ', this.signInForm);
+      swal('Error', 'Verifica la información del formulario', 'error');
+    }
+  }
 }
