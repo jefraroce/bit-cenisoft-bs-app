3b729ef0d0b4f393ea61e84080aa3acf78bf66b2
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2021-02-25 22:20:41 -0500

    Adjusted app layouts and routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,57 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { WithBannerComponent } from './layouts/with-banner/with-banner.component';
+import { WithoutBannerComponent } from './layouts/without-banner/without-banner.component';
 import { BookComponent } from './pages/book/book.component';
+import { CartComponent } from './pages/cart/cart.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    component: WithBannerComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'sign-in',
+        component: SignInComponent
+      },
+      {
+        path: 'sign-up',
+        component: SignUpComponent
+      }
+    ]
   },
   {
-    path: 'home',
-    component: HomeComponent
+    path: '',
+    component: WithoutBannerComponent,
+    children: [
+      {
+        path: 'books/:id',
+        component: BookComponent
+      },
+      {
+        path: 'cart',
+        canActivate: [AuthGuard],
+        component: CartComponent
+      }
+    ]
   },
   {
-    path: 'books/:id',
-    component: BookComponent
-  },
-  {
-    path: 'sign-in',
-    component: SignInComponent
-  },
-  {
-    path: 'sign-up',
-    component: SignUpComponent
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 
---
+++ src/app/layouts/with-banner/with-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-with-banner',
+  templateUrl: './with-banner.component.html',
+  styleUrls: ['./with-banner.component.scss']
+})
+export class WithBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
